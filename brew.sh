9f1155fab097b365191474575912cd9db409fc61
seanucla <112214371+seanucla@users.noreply.github.com>
2022-09-19 14:13:07 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Install MacOS Applications
-brew --install cask google-chrome
-brew --install cask firefox
-brew --install cask box-drive
-brew --install cask microsoft-auto-update
-brew --install cask microsoft-excel
-brew --install cask microsoft-outlook
-brew --install cask microsoft-powerpoint
-brew --install cask microsoft-remote-desktop
-brew --install cask microsoft-teams
-brew --install cask microsoft-word
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask box-drive
+brew install --cask microsoft-auto-update
+brew install --cask microsoft-excel
+brew install --cask microsoft-outlook
+brew install --cask microsoft-powerpoint
+brew install --cask microsoft-remote-desktop
+brew install --cask microsoft-teams
+brew install --cask microsoft-word
 brew install --cask adobe-creative-cloud
-brew --install cask adobe-acrobat-pro
-brew --install cask zoom
+brew install --cask adobe-acrobat-pro
+brew install --cask zoom
 
 # Install Utilities
 brew install dockutil
